ad586726620866661660ea2bec7a35f5f8f5b543
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-04-04 13:43:30 +0200

    input form for new shortcuts

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-const fs = require('fs');
+const input__form = document.getElementById('input__form');
+const input__keys = document.getElementById('input__keys');
+const input__description = document.getElementById('input__description');
+console.log('my console log: input__form', input__form);
 
-const text = fs.readFileSync('ksiega.txt','utf8');
-console.log(text);
-
-const para = document.getElementById('para');
-para.innerText = text;
+input__form.addEventListener('submit', e =>{
+    e.preventDefault();
+    console.log(input__keys.value);
+    console.log(input__description.value);
+})
+class Shortcut{
+    constructor(keys, description){
+        this.keys = keys;
+        this.description = description;
+    }
+    getKeys(){
+        return this.keys
+    }
+    getDescription(){
+        return this.description
+    }
+}
+const shortCut1 = new Shortcut('Ctrl+`','Show integrated terminal');
+console.log(shortCut1.getKeys());
+console.log(shortCut1.getDescription());
+localStorage.setItem(shortCut1.getKeys(),JSON.stringify(shortCut1));
