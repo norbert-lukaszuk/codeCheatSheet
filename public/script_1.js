80b2c7438918343ab3a3af2a5f8539b5097f73f6
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-04-09 22:39:57 +0200

    async data issue

--- public/script_1.js
+++ public/script_1.js
@@ -2,7 +2,7 @@ const output = document.getElementById('output__container');
 db.collection("jsSnippets").get()
 .then(snapshot=>{
     console.log(snapshot.docs[0].data().code);
-    const snippet = (snapshot.docs[0].data().code.toString());
+    snippet = (snapshot.docs[0].data().code.toString());
     const prism_el = document.createElement('pre');
     const code = document.createElement('code');
     code.className = 'language-js';
