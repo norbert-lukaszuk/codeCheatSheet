ea1b2d2fe88388e47f03a66eb869192ed1da1216
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-04-09 21:03:02 +0200

    trying to get snippets from db colorized

---
+++ public/script_1.js
@@ -0,0 +1,14 @@
+const output = document.getElementById('output__container');
+db.collection("jsSnippets").get()
+.then(snapshot=>{
+    console.log(snapshot.docs[0].data().code);
+    const snippet = (snapshot.docs[0].data().code.toString());
+    const prism_el = document.createElement('pre');
+    const code = document.createElement('code');
+    code.className = 'language-js';
+    console.log(code.childNodes);
+    prism_el.append(code);
+    code.innerText = snippet;
+    console.log(prism_el.childNodes);
+    output.append(prism_el);
+})
