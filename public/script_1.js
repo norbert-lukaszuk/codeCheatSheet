7c8f9d14db4893876e1900baa9d54435b09e4a7c
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-04-10 19:14:58 +0200

    input textarea inplementation

--- public/script_1.js
+++ public/script_1.js
@@ -1,18 +1,27 @@
 const output = document.getElementById('output__container');
+const add__button = document.getElementById('add__button');
+const input__textarea = document.getElementById('input__textarea');
+add__button.addEventListener('click', e=>{
+    input__textarea.classList.toggle('input__textarea--hide');
+})
 db.collection("jsSnippets").get()
 .then(snapshot=>{
-    console.log(snapshot.docs[0].data().code);
-    snippet = (snapshot.docs[0].data().code);
-    const prism_el = document.createElement('pre');
-    const code = document.createElement('code');
-    code.className = 'language-js';
-    code.id = 'snippet';
-    console.log(code);
-    code.innerText = snippet;
-    code.innerHTML.replace(/<br \/>/g,'\n')
-    console.log(prism_el);
-    prism_el.append(code);
-    output.append(prism_el);
+    const docs = snapshot.docs;
+    docs.forEach(e=>{
+        for(let i=0; i<=5; i++){
+
+            const snippet = e.data().code;
+            const description = e.data().description;
+            output.innerHTML += `<h4 class="snippet__header">${description}</h4>`
+            const prism_el = document.createElement('pre');
+            const code = document.createElement('code');
+            code.className = 'language-js';
+            code.innerText = snippet;
+            prism_el.append(code);
+            output.append(prism_el);
+        }
+
+    })
     Prism.highlightAll();
 
 })
