11ccac873fddc87a2afb0144423a56ffb8919b85
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-04-10 20:28:59 +0200

    sending code & description to firestore

--- public/script_1.js
+++ public/script_1.js
@@ -1,9 +1,15 @@
 const output = document.getElementById('output__container');
 const add__button = document.getElementById('add__button');
+const description__textarea = document.getElementById('description__textarea');
 const input__textarea = document.getElementById('input__textarea');
+const send__button = document.getElementById('send__button');
+const cancel__button = document.getElementById('cancel__button');
+// open input for snippet
 add__button.addEventListener('click', e=>{
-    input__textarea.classList.toggle('input__textarea--hide');
+    toggleInput();
+    
 })
+// loading code snippets from firestore
 db.collection("jsSnippets").get()
 .then(snapshot=>{
     const docs = snapshot.docs;
@@ -20,8 +26,38 @@ db.collection("jsSnippets").get()
             prism_el.append(code);
             output.append(prism_el);
         }
-
     })
     Prism.highlightAll();
-
 })
+// getting the input & sending it to the firestore
+
+send__button.addEventListener('click', e=>{
+    const snippet = input__textarea.value;
+    const description = description__textarea.value;
+    console.log(snippet);
+    toggleInput();
+
+    // sending to firestore
+    db.collection("jsSnippets").add({
+        code: snippet,
+        description: description
+    })
+    .catch(err=>console.error(err))
+    
+})
+// canceling input 
+cancel__button.addEventListener('click', e=>{
+    toggleInput();
+    
+})
+// toggling whole visibility input function
+function toggleInput() {
+    input__textarea.classList.toggle('input__textarea--hide');
+    description__textarea.classList.toggle('input__textarea--hide');
+    send__button.classList.toggle('send__button--hide');
+    cancel__button.classList.toggle('cancel__button--hide');
+    input__textarea.value = '';
+    description__textarea.value = '';
+
+
+}
