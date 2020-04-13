04b527ddbede183a25ce3735adfda1ef0d9e30cc
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-04-13 12:04:15 +0200

    alert when language is not selected + styling

--- public/script_1.js
+++ public/script_1.js
@@ -46,19 +46,6 @@ navigation__list.addEventListener('click', e=>{
     }
 })
 
-// real time listener for firestore
-
-// db.collection('data/codeSnippets/gitSnippets/').onSnapshot(snapshot=>{
-//     snapshot.docChanges().forEach(e=>{
-//         console.log(e);
-//         if(e.type === 'added'){
-//             console.log(`added doc ${e.doc.id}`);
-//             console.log(`data: ${e.doc.data().code}`);
-
-//         }
-//     })
-// })
-
 db.collection(`data/codeSnippets/${category__selected}/`).orderBy('description').onSnapshot(snapshot=>{
     snapshot.docChanges().forEach(e=>{
         if(e.type === 'added'){
@@ -76,52 +63,17 @@ db.collection(`data/codeSnippets/${category__selected}/`).orderBy('description')
     })
     Prism.highlightAll();
 })
-// get data from subcollection
-db.collection('data').doc('codeSnippets').collection('jsSnippets').get()
-.then(snapshot=>{
-    console.log(snapshot.docs)
-})
-.catch(err=>console.log(err)) 
-// geting data from bouth collections
-let arr = ['jsSnippets','gitSnippets']
-arr.forEach(e=>{
-    console.log(e)
-    db.collection(e).get()
-    .then(snapshot=>{
-    const docs = snapshot.docs;
-    docs.forEach(e=>{
-    const snippet = e.data().code;
-    // console.log(snippet);
-    
-    })
-    })
-    .catch(err=>console.log(err))
-    
-    })
-// loading code snippets from firestore
-// db.collection("jsSnippets").get()
-// .then(snapshot=>{
-//     const docs = snapshot.docs;
-//     docs.forEach(e=>{
-//         const snippet = e.data().code;
-//         const description = e.data().description;
-//         output.innerHTML += `<h4 class="snippet__header">${description}</h4>`
-//         const prism_el = document.createElement('pre');
-//         const code = document.createElement('code');
-//         code.className = 'language-js';
-//         code.innerText = snippet;
-//         prism_el.append(code);
-//         output.append(prism_el);
-//     })
-//     Prism.highlightAll();
-// })
-// getting the input & sending it to the firestore
 
+// send procedure 
 send__button.addEventListener('click', e=>{
     const snippet = input__textarea.value;
     const description = description__textarea.value;
     const category = checkCategory();
     console.log(category);
+    if(category === undefined){
+        alert('Select language of snippet')
+    }
+    else{
     toggleInput();
 
     // sending to firestore
@@ -131,7 +83,7 @@ send__button.addEventListener('click', e=>{
         description: description
     })
     .catch(err=>console.error(err))
-    
+    }
 })
 // canceling input 
 cancel__button.addEventListener('click', e=>{
@@ -145,8 +97,11 @@ function toggleInput() {
     send__button.classList.toggle('send__button--hide');
     cancel__button.classList.toggle('cancel__button--hide');
     category__wraper.classList.toggle('category__wraper--hide');
+    navigation__list.classList.toggle('navigation__list--hidden'); // hide main menu
     input__textarea.value = '';
     description__textarea.value = '';
+    // reset radio buttons in input
+    category.forEach(e=>e.checked = false);
 }
 // check witch category radio input is selected
 function checkCategory(){
